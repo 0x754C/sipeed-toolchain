df02b0cbe82b574e68e6950c979f58fa5278cda2
Lu Hui <luhui@sipeed.com>
2024-02-28 19:30:25 +0800

    buildroot-sipeed: add basic licheervnano support

---
+++ buildroot-sipeed/board/licheervnano/post-image.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -eux
+
+BOARD_DIR="$(dirname $0)"
+BOARD_NAME="$(basename ${BOARD_DIR})"
+GENIMAGE_CFG="${BOARD_DIR}/genimage.cfg"
+GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
+
+cd ${BINARIES_DIR}
+lzma -c -5 -f -k Image > Image.lzma
+cp ${BOARD_DIR}/multi.its ./
+mkimage -f multi.its boot.sd
+
+trap 'rm -rf "${ROOTPATH_TMP}"' EXIT
+ROOTPATH_TMP="$(mktemp -d)"
+
+rm -rf "${GENIMAGE_TMP}"
+
+genimage \
+	--rootpath "${ROOTPATH_TMP}"   \
+	--tmppath "${GENIMAGE_TMP}"    \
+	--inputpath "${BINARIES_DIR}"  \
+	--outputpath "${BINARIES_DIR}" \
+	--config "${GENIMAGE_CFG}"
+
+exit $?
